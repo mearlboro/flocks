0903499e5aefff85929189d8b88e4c7f0e84b5cd
mis <mearlboro@protonmail.com>
2023-02-06 13:26:25 +0000

    [feat] improve gif creation
    - add support for creating gifs from arbitrary indexes
    - add palette for file compression
    - custom file size

--- out/gif.sh
+++ out/gif.sh
@@ -8,15 +8,24 @@ if [ ! -d "$indir" ]; then
     exit
 fi
 
-if [ ! -d "$outdir"]; then
+if [ ! -d "$outdir" ]; then
     mkdir -p -v $outdir
 fi
 
+
 for dir in $indir/*; do
-    [ -d "$dir" ] || continue
+    [ -d "$dir" ] || continue;
 
     name=$(basename $dir)
+    first_file=`find $dir -type f | sort | head -n 1`
+    start_at=`echo $(basename $first_file) | tr -cd '[0-9]'`
 
-    echo "Creating gif from $dir to $outdir/$name.gif"
-    ffmpeg -framerate 12 -i $dir/%d.jpg  -r 24 $outdir/$name.gif
+    echo "Creating gif from $dir to $outdir/$name.gif starting at $start_at"
+
+    # create a pallette for GIF compression
+    ffmpeg -y -i "$first_file" -vf palettegen "$dir/palette.png"
+
+    ffmpeg -framerate 12 -start_number "$start_at" -i "$dir"/%d.jpg -i "$dir/palette.png" -lavfi paletteuse -r 24 -s 800x600 "$outdir/$name.gif"
+
+    rm "$dir/palette.png"
 done
