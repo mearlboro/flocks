1c649cabf7570853cbdede9f220362a2639bbf2f
Dragos Dumitrache <dragosd2000@gmail.com>
2023-07-22 14:11:03 +0100

    Add CI job for publishing a package

--- tests/utils/test_utils.py
+++ tests/utils/test_utils.py
@@ -4,8 +4,9 @@ from math import pi, sqrt
 import matplotlib.cm as cm
 import numpy as np
 import pytest
-from hamcrest import assert_that, close_to
+from hamcrest import assert_that, close_to, is_, equal_to
 from matplotlib import pyplot as plt
+
 from pyflocks.util.geometry import vec_to_ang, ang_to_vec, neighbours, EnumNeighbours, EnumBounds, average_angles, \
     sum_vec_ang, centre_of_mass, periodic_diff, ang_mod, bearing_to
 from pyflocks.util.plot import plot_vector
@@ -58,54 +59,41 @@ def test_scalar_e(i, actual, expected) -> None:
     assert_that(actual, close_to(expected, 1e-5), f"Test {i}: failed! Expected {expected}, got {actual}")
 
 
+@pytest.mark.parametrize('i,actual,expected', [
+    9, ang_to_vec(0), [1, 0],
+    10, ang_to_vec(pi), [-1, 0],
+    11, ang_to_vec(-pi), [-1, 0],
+    12, ang_to_vec(pi / 2), [0, 1],
+    13, ang_to_vec(-pi / 2), [0, -1],
+    14, ang_to_vec(pi / 4), [1 / sqrt(2), 1 / sqrt(2)],
+    15, ang_to_vec(3 * pi / 4), [-1 / sqrt(2), 1 / sqrt(2)],
+    20, neighbours(1, X, 1, EnumNeighbours.METRIC, EnumBounds.REFLECTIVE), [1, 2, 3],
+    21, neighbours(0, X, 0.11, EnumNeighbours.METRIC, EnumBounds.REFLECTIVE), [0, 5],
+    22, neighbours(0, X, 0.1, EnumNeighbours.METRIC, EnumBounds.REFLECTIVE), [0],
+    23, neighbours(1, X, 3, EnumNeighbours.TOPOLOGICAL, EnumBounds.REFLECTIVE), [1, 3, 2, 4],
+    24, neighbours(0, X, 3, EnumNeighbours.TOPOLOGICAL, EnumBounds.REFLECTIVE), [0, 5, 4, 3],
+    25, neighbours(1, X, 1, EnumNeighbours.METRIC, EnumBounds.PERIODIC, 5), [1, 2, 3],
+    26, neighbours(0, X, 1, EnumNeighbours.METRIC, EnumBounds.PERIODIC, 5), [0, 5],
+    27, neighbours(1, X, 5, EnumNeighbours.TOPOLOGICAL, EnumBounds.PERIODIC, 5), [1, 3, 2, 4, 6, 0],
+    28, neighbours(0, X, 5, EnumNeighbours.TOPOLOGICAL, EnumBounds.PERIODIC, 5), [0, 5, 4, 7, 8, 6],
+    32, sum_vec_ang(A, V), np.mean([ang_to_vec(a) * v for a in A], axis=0),
+    34, centre_of_mass(X, l, EnumBounds.PERIODIC), centre_of_mass(X1, l, EnumBounds.PERIODIC),
+    35, centre_of_mass(X2, l, EnumBounds.PERIODIC), [9.95, 9.95],
+    36, centre_of_mass(X2, l, EnumBounds.REFLECTIVE), [4.95, 4.95],
+    37, periodic_diff(X[0], X[1], l), np.array([2, 2]),
+    38, periodic_diff(X[0], X[2], l), np.array([0, -1]),
+    39, periodic_diff(X[2], X[0], l), np.array([0, 1]),
+])
 def test_vector_e(i, actual, expected) -> None:
-    if len(actual) != len(expected) or any(
-            [abs(a - e) > 1e-5 for (a, e) in zip(actual, expected)]):
-        print(f"Test {i}: failed! Expected {expected}, got {actual}")
+    assert_that(len(actual), is_(equal_to(len(expected))))
+    assert_that(any(abs(a - e) > 1e-5 for a, e, in zip(actual, expected)), is_(equal_to(False)),
+                f"Test {i}: failed! Expected {expected}, got {actual}")
 
 
 def test_np_array(i, actual, expected) -> None:
     if not np.array_equal(actual, expected):
         print(f"Test {i}: failed! Expected {expected}, got {actual}")
 
-
-test_vector_e(9, ang_to_vec(0), [1, 0])
-test_vector_e(10, ang_to_vec(pi), [-1, 0])
-test_vector_e(11, ang_to_vec(-pi), [-1, 0])
-test_vector_e(12, ang_to_vec(pi / 2), [0, 1])
-test_vector_e(13, ang_to_vec(-pi / 2), [0, -1])
-test_vector_e(14, ang_to_vec(pi / 4), [1 / sqrt(2), 1 / sqrt(2)])
-test_vector_e(15, ang_to_vec(3 * pi / 4), [-1 / sqrt(2), 1 / sqrt(2)])
-
-# test neighbours in reflective bounds
-X = np.array([[4, 4], [2, 2], [2, 2.8], [2.5, 2.5], [3, 3], [4, 4.1], [0.1, 0.1], [0, 5], [0, 0]])
-test_vector_e(20, neighbours(1, X, 1, EnumNeighbours.METRIC, EnumBounds.REFLECTIVE), [1, 2, 3])
-test_vector_e(21, neighbours(0, X, 0.11, EnumNeighbours.METRIC, EnumBounds.REFLECTIVE), [0, 5])
-# will fail because of floating-point prcision; implement Decimal instead for this
-test_vector_e(22, neighbours(0, X, 0.1, EnumNeighbours.METRIC, EnumBounds.REFLECTIVE), [0])
-
-test_vector_e(23, neighbours(1, X, 3, EnumNeighbours.TOPOLOGICAL, EnumBounds.REFLECTIVE), [1, 3, 2, 4])
-test_vector_e(24, neighbours(0, X, 3, EnumNeighbours.TOPOLOGICAL, EnumBounds.REFLECTIVE), [0, 5, 4, 3])
-
-# test neighbours in periodic bounds
-test_vector_e(25, neighbours(1, X, 1, EnumNeighbours.METRIC, EnumBounds.PERIODIC, 5), [1, 2, 3])
-test_vector_e(26, neighbours(0, X, 1, EnumNeighbours.METRIC, EnumBounds.PERIODIC, 5), [0, 5])
-
-test_vector_e(27, neighbours(1, X, 5, EnumNeighbours.TOPOLOGICAL, EnumBounds.PERIODIC, 5), [1, 3, 2, 4, 6, 0])
-test_vector_e(28, neighbours(0, X, 5, EnumNeighbours.TOPOLOGICAL, EnumBounds.PERIODIC, 5), [0, 5, 4, 7, 8, 6])
-
-test_vector_e(32, sum_vec_ang(A, V),
-              np.mean([ang_to_vec(a) * v for a in A], axis=0) * n)
-
-test_vector_e(34, centre_of_mass(X, l, EnumBounds.PERIODIC), centre_of_mass(X1, l, EnumBounds.PERIODIC))
-test_vector_e(35, centre_of_mass(X2, l, EnumBounds.PERIODIC), [9.95, 9.95])
-test_vector_e(36, centre_of_mass(X2, l, EnumBounds.REFLECTIVE), [4.95, 4.95])
-
-test_vector_e(37, periodic_diff(X[0], X[1], l), np.array([2, 2]))
-test_vector_e(38, periodic_diff(X[0], X[2], l), np.array([0, -1]))
-test_vector_e(39, periodic_diff(X[2], X[0], l), np.array([0, 1]))
-
-print("If nothing so far was printed, it means your tests all pass! Good job.")
 
 # show graphically the angles and plotting function
 col = cm.rainbow(np.linspace(0, 1, 30))
